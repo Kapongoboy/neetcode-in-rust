6958e51358c902729e9aeb4c899b66fe07ea99fc
Plamedi Luis <plamedi.luis11@gmail.com>
2024-04-05 14:53:45 +0200

    completed two_sum

--- src/arrays_and_hashing.rs
+++ src/arrays_and_hashing.rs
@@ -27,6 +27,19 @@ pub fn is_anagram(s: String, t: String) -> bool {
     m_s == m_t
 }
 
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut m: HashMap<i32, usize> = HashMap::new();
+    for (i, num) in nums.iter().enumerate() {
+        let pair = target - num;
+        match m.get(&pair) {
+            Some(idx) => return vec![*idx as i32, i as i32],
+            None => m.insert(*num, i),
+        };
+    }
+
+    vec![0]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,5 +74,29 @@ mod tests {
         let s = String::from("rat");
         let t = String::from("car");
         assert!(!is_anagram(s, t));
+    }
+
+    #[test]
+    fn two_sums_example_1() {
+        let input = vec![2, 7, 11, 15];
+        let target = 9;
+        let output = vec![0, 1];
+        assert_eq!(output, two_sum(input, target));
+    }
+
+    #[test]
+    fn two_sums_example_2() {
+        let input = vec![3, 2, 4];
+        let target = 6;
+        let output = vec![1, 2];
+        assert_eq!(output, two_sum(input, target));
+    }
+
+    #[test]
+    fn two_sums_example_3() {
+        let input = vec![3, 3];
+        let target = 6;
+        let output = vec![0, 1];
+        assert_eq!(output, two_sum(input, target));
     }
 }
