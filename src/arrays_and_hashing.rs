6aa6c1190d9eebf4cdc0e1c747f3f92aabfc7f79
Plamedi Luis <plamedi.luis11@gmail.com>
2024-04-05 13:32:26 +0200

    a bit faster now but still a lot of memory

--- src/arrays_and_hashing.rs
+++ src/arrays_and_hashing.rs
@@ -3,14 +3,13 @@ use std::collections::HashMap;
 pub fn contains_duplicate(nums: Vec<i32>) -> bool {
     let mut m = HashMap::new();
     for num in nums.iter() {
-        m.entry(num).and_modify(|x| *x += 1).or_insert(1);
+        match m.get(num) {
+            Some(_) => return true,
+            None => m.insert(num, 0),
+        };
     }
 
-    let mut result = m.iter().filter(|v| *v.1 > 1);
-    match result.next() {
-        Some(_) => true,
-        None => false,
-    }
+    false
 }
 
 #[cfg(test)]
@@ -20,18 +19,18 @@ mod tests {
     #[test]
     fn contains_duplicate_example_1() {
         let nums = vec![1, 2, 3, 1];
-        assert_eq!(true, contains_duplicate(nums))
+        assert!(contains_duplicate(nums))
     }
 
     #[test]
     fn contains_duplicate_example_2() {
         let nums = vec![1, 2, 3, 4];
-        assert_eq!(false, contains_duplicate(nums))
+        assert!(!contains_duplicate(nums))
     }
 
     #[test]
     fn contains_duplicate_example_3() {
         let nums = vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
-        assert_eq!(true, contains_duplicate(nums))
+        assert!(contains_duplicate(nums))
     }
 }
